ae9dd7677aac00fef6f8bdf845e5421409f9c916
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-10-05 09:11:39 +0200

    better poc explanation

--- python/import/import04_several_shades_of_from_X_import_Y/poc.py
+++ python/import/import04_several_shades_of_from_X_import_Y/poc.py
@@ -7,9 +7,14 @@ from mysuperpackage import sub  # subpackage
 print(
     """
     This POC shows that the synxax 'from X import Y' may be used to import 3 different things :
-        - Y is the name of an object in the module X
-        - Y is a module in the package X
-        - Y is a subpackage in the package X (in which case names are pulled from its __init__.py)
+        1. Y is the name of an object in the module X
+        2. Y is a module in the package X
+        3. Y is a subpackage in the package X (in which case names are pulled from its __init__.py)
+
+    Said otherwise : if we are interested in the module containing the imported code, it could be :
+        1. X.py
+        2. X/Y.py
+        3. X/Y/__init__.py
 
     See:
         https://docs.python.org/3/tutorial/modules.html#packages
