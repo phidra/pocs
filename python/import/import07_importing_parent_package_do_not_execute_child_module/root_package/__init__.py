694996eb0038da4eaeb5a817b729c977cff0dd40
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-10-07 14:40:48 +0200

    poc on package importing

---
+++ python/import/import07_importing_parent_package_do_not_execute_child_module/root_package/__init__.py
@@ -0,0 +1,5 @@
+print("=== If you see this message, root_package/__init__.py was executed")
+
+
+def root_package_function():
+    print("I am a function in the PARENT package")
---
+++ python/import/import07_importing_parent_package_do_not_execute_child_module/root_package/child_module.py
@@ -0,0 +1,5 @@
+print("=== If you see this message, root_package/child_module.py was executed")
+
+
+def child_module_function():
+    print("I am a function in the CHILD MODULE")
