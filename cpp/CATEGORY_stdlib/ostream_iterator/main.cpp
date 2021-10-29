4dca2b5f2904a8abc2eedb2c5296892b93c09d38
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-10-29 14:59:25 +0200

    renaming

--- cpp/CATEGORY_stdlib/ostream_iterator/main.cpp
+++ cpp/CATEGORY_stdlib/ostream_iterator/main.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
-
     cout << R"DELIM(
 This POC show the use of std::ostream_iterator to easily display a vector.
 
@@ -17,11 +16,10 @@ Cf. https://en.cppreference.com/w/cpp/iterator/ostream_iterator
     auto items = vector<int>{1, 3, 5, 7, 9};
 
     cout << "with explicit for-loop :" << endl;
-    for (auto item: items) {
+    for (auto item : items) {
         cout << item << "\n";
     }
     cout << endl;
-
 
     cout << "with ostream_iterator :" << endl;
     copy(items.begin(), items.end(), ostream_iterator<int>(cout, "\n"));
