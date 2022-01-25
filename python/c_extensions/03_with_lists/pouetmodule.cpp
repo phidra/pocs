2ae11378579d4cb39a08fdf55e27dd7add928c4c
Phidra <phidra@pm.me>
2022-01-25 21:18:12 +0100

    POC on cpp extension with lists

---
+++ python/c_extensions/03_with_lists/pouetmodule.cpp
@@ -0,0 +1,56 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+static PyObject* pouet_double_the_ints(PyObject* self, PyObject* args) {
+    PyObject* input_list;
+    if (!PyArg_ParseTuple(args, "O!", &PyList_Type, &input_list)) {
+        return NULL;
+    }
+    Py_ssize_t length = PyList_GET_SIZE(input_list);
+
+    PyObject* my_returned_list = PyList_New(length);
+    if (!my_returned_list) {
+        return NULL;
+    }
+
+    for (Py_ssize_t i = 0; i < length; ++i) {
+        PyObject* item = PyList_GetItem(input_list, i);
+
+        // is the item an integer (or convertible to it) ?
+        long item_long = PyLong_AsLong(item);
+        if (PyErr_Occurred()) {
+            Py_DECREF(my_returned_list);
+            return NULL;
+        }
+
+        // ignoring overflow errors for now...
+        PyObject* doubled = PyLong_FromLong(2 * item_long);
+        if (!doubled) {
+            Py_DECREF(my_returned_list);
+            return NULL;
+        }
+
+        if (!PyList_SET_ITEM(my_returned_list, i, doubled)) {
+            Py_DECREF(my_returned_list);
+            return NULL;
+        }
+    }
+
+    return my_returned_list;
+}
+
+static PyMethodDef PouetMethods[] = {
+    {"double_the_ints", pouet_double_the_ints, METH_VARARGS, "Double ints in a list"},
+    {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+static struct PyModuleDef pouetmodule = {PyModuleDef_HEAD_INIT, "pouet", NULL, -1, PouetMethods};
+
+PyMODINIT_FUNC PyInit_pouet(void) {
+    return PyModule_Create(&pouetmodule);
+}
