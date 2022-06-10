d622ff68edd31be5370d781334c17248b7883aae
Philippe DRABCZUK <philippe@drabczuk.fr>
2022-06-10 15:11:58 +0200

    better POC launch

--- js/leaflet_saving_map_config_in_url/build_and_run.sh
+++ js/leaflet_saving_map_config_in_url/build_and_run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+this_script_parent="$(realpath "$(dirname "$0")" )"
+
 echo ""
-echo "Ce script lance un serveur pour servir la POC leaflet (nécessaire, car j'utilise des modules, ce qui pose des problèmes CORS)"
-echo "Derrière, il faut charger l'URL suivante dans un browser :"
+echo "Ce script lance un serveur pour servir la POC leaflet (nécessaire, car j'utilise des modules js)"
+echo "Derrière, il faut charger l'URL indiquée par le serveur dans un browser."
+echo "    python3 -m http.server --directory='${this_script_parent}/dist/' 8787"
 echo ""
-echo "     http://localhost:8000/index.html"
-echo ""
-python -m SimpleHTTPServer
+echo "(derrière, le serveur indique l'URL à requêter pour utiliser le site)"
+python3 -m http.server --directory="${this_script_parent}" 8787
