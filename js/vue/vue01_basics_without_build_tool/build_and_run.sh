41f23f049a6cf643447019a2c0fdc19c8f83d037
Phidra <phidra@pm.me>
2022-06-10 11:55:31 +0200

    POC howto for vue01

---
+++ js/vue/vue01_basics_without_build_tool/build_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo ""
+echo "Pour cette POC, inutile de lancer un serveur, simplement ouvrir le fichier avec un browser suffit :"
+echo ""
+echo "     firefox ./basics.html"
+echo ""
