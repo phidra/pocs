d69f8771c3284baf3953baeb95d6ec5d871dd02f
Philippe DRABCZUK <philippe@drabczuk.fr>
2022-06-10 18:12:01 +0200

    WIP my own marker component for using leaflet in vue

---
+++ js/vue/vue06_leaflet_with_my_own_marker_component/src/MyOwnMarker.vue
@@ -0,0 +1,24 @@
+<script>
+import L from "leaflet";
+export default {
+  name: "Cell",
+  data() {
+    return {
+      lat: 0,
+      lng: 0,
+      marker_object: null,
+    }
+  },
+  inject: ['map', 'message'],
+  mounted: function() {
+    alert(`message is ${this.message}, est-ce que la map est null ? ${this.map === null}`);
+    L.marker([42, 1], {draggable: true}).addTo(this.map);
+  },
+  methods: {
+    on_start_new_game() {
+      this.lat = 42;
+      this.lng = 1;
+    },
+  },
+}
+</script>
