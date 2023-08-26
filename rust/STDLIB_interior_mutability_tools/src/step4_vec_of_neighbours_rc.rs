6f4e486744bdd6749dac2ea8313ec590f781fb82
Phidra <phidra@pm.me>
2023-08-26 15:08:46 +0200

    notes de reprise

--- rust/STDLIB_interior_mutability_tools/src/step4_vec_of_neighbours_rc.rs
+++ rust/STDLIB_interior_mutability_tools/src/step4_vec_of_neighbours_rc.rs
@@ -4,6 +4,11 @@ Cette step4 remplace les références par des Rc pour stocker les neighbours.
 
 WORK IN PROGRESS
 
+REPRISE = faire en sorte que chaque node stocke un Rc de Node
+
+(montrer que ça règle une partie des soucis, mais que comme les Rc sont immutables, ils nous manque RefCell)
+(puis, faire l'étape 5 = étape finale = on stocke chaque Neighbour comme Rc<RefCell<T>>)
+
 
 "#;
 
