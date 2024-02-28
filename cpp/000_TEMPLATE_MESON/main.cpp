713a6ef9d0a753f03399efca0712f8041f04585b
Phidra <phidra@pm.me>
2024-02-28 20:34:59 +0100

    Update du template cpp meson pour ajouter la poc_description

--- cpp/000_TEMPLATE_MESON/main.cpp
+++ cpp/000_TEMPLATE_MESON/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
-int main(void) {
-    std::cout << "pouet" << std::endl;
-    return 0;
+
+using std::cout;
+using std::endl;
+
+void print_poc_description() {
+    cout << R"DELIM(
+CE QUE MONTRE CETTE POC = TODO TODO TODO
+
+- TODO
+- TODO
+- TODO
+- TODO
+
+TODO
+
+)DELIM";
+    cout << endl;
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+    print_poc_description();
+
+    cout << "TODO" << endl;
+    cout << "TODO" << endl;
+    cout << "TODO" << endl;
 }
