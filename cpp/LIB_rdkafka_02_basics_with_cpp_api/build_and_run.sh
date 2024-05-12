c0264dba66e5cf3d4526564c6f4fa894f10abf81
Phidra <phidra@pm.me>
2024-05-12 21:14:52 +0200

    POC kafka utilisant l'API C++

---
+++ cpp/LIB_rdkafka_02_basics_with_cpp_api/build_and_run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#
+set -o errexit
+set -o nounset
+set -o pipefail
+
+this_script_parent="$(realpath "$(dirname "$0")" )"
+echo "parent = $this_script_parent" > /dev/null
+
+
+cat << EOF
+
+
+Cette POC est une simplification de cet exemple de la lib :
+    https://github.com/confluentinc/librdkafka/blob/2587cac70f83fced42c51f921bed325a434f5bc7/examples/rdkafka_example.cpp
+
+Elle démontre l'utilisation de l'API C++ de librdkafka pour envoyer/recevoir des messages sur un topic.
+
+Quelques enseignements :
+
+- il y a une doc Doxygen de la librairie :
+    https://docs.confluent.io/platform/current/clients/librdkafka/html/index.html
+- on dirait que le producer doit appeler poll régulièrement :
+    https://github.com/confluentinc/librdkafka/blob/2587cac70f83fced42c51f921bed325a434f5bc7/src-cpp/rdkafkacpp.h#L1533
+- conservation des messages par kafka :
+    - avec cette POC, kafka garde en mémoire tous les events émis.
+    - par ailleurs, le consumer est configuré pour lire tous les messages depuis le début
+    - donc si kafka a des messages, à chaque fois qu'on lancera un consumer, on récupèrera tous ces "anciens" messages
+    - dit autrement, ça n'est pas du pub/sub
+
+EOF
+
+
+LIBRDKAFKA_INSTALL_DIR="${this_script_parent}/NOGIT_install_librdkafka"
+
+# VÉRIFICATION QUE LIBRDKAFKA A ÉTÉ INSTALLÉE :
+if [ ! -d "$LIBRDKAFKA_INSTALL_DIR" ]
+then
+    cat << EOF
+
+ERREUR : cette POC nécessite d'avoir installé manuellement librdkafka, comme suit :
+
+# NOTE : la POC a été testée avec la branche v2.4.0 de kafka, donc je clone cette version (à bumper si besoin)
+git clone --depth=1 --shallow-submodules --branch=v2.4.0 https://github.com/confluentinc/librdkafka.git NOGIT_librdkafka
+cd NOGIT_librdkafka
+cmake -H. -B_cmake_build
+ccmake _cmake_build
+    modifier CMAKE_INSTALL_PREFIX pour indiquer ce chemin : $LIBRDKAFKA_INSTALL_DIR
+    (i.e. indiquer /path/to/poc/NOGIT_install_librdkafka)
+    puis, choisir "configure" puis "quit"
+cmake --build _cmake_build
+cmake --build _cmake_build --target install
+    vérifier dans les logs qu'on a bien installé librdkafka au bon endroit :
+    -- Installing: (...)
+    -- Installing: /path/to/poc/NOGIT_install_librdkafka/lib/librdkafka++.so
+    -- Installing: /path/to/poc/NOGIT_install_librdkafka/include/librdkafka/rdkafkacpp.h
+    -- Installing: (...)
+cd ..
+
+EOF
+    exit 1
+fi
+
+
+# COMPILATION :
+echo "Compilation du producer et du consumer :"
+rm -rf NOGIT_build
+ CXX=clang++-14 meson setup NOGIT_build/ .
+ CXX=clang++-14 meson compile  -C NOGIT_build/
+
+
+# INSTRUCTIONS :
+cat << EOF
+...les binaires ont maintenant été compilés, reste à lancer la POC manuellement, dans plusieurs terminaux :
+
+STEP 0 = dans un premier terminal, lancer :
+    (si besoin : docker container prune)
+    docker-compose up
+
+STEP 1 = dans un second terminal, produire les messages :
+    LD_LIBRARY_PATH=NOGIT_install_librdkafka/lib ./NOGIT_build/bin_producer TARTIFLETTE  msg1  msg2  msg99
+
+STEP 2 = consommer les messages :
+    LD_LIBRARY_PATH=NOGIT_install_librdkafka/lib ./NOGIT_build/bin_consumer TARTIFLETTE
+
+EOF
---
+++ cpp/LIB_rdkafka_02_basics_with_cpp_api/src/main_consumer.cpp
@@ -0,0 +1,164 @@
+#include <librdkafka/rdkafkacpp.h>
+
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+static volatile sig_atomic_t run = 1;
+static bool exit_eof = false;
+
+static void sigterm([[maybe_unused]] int sig) {
+    run = 0;
+}
+
+void msg_consume(RdKafka::Message* message) {
+    RdKafka::Headers const* headers;
+
+    switch (message->err()) {
+        case RdKafka::ERR__TIMED_OUT:
+            break;
+
+        case RdKafka::ERR_NO_ERROR:
+            /* Real message */
+            std::cout << "\nRead msg at offset " << message->offset() << std::endl;
+            if (message->key()) {
+                std::cout << "Key: " << *message->key() << std::endl;
+            }
+            headers = message->headers();
+            if (headers) {
+                std::vector<RdKafka::Headers::Header> hdrs = headers->get_all();
+                for (size_t i = 0; i < hdrs.size(); i++) {
+                    const RdKafka::Headers::Header hdr = hdrs[i];
+
+                    if (hdr.value() != NULL)
+                        printf(" Header: %s = \"%.*s\"\n",
+                               hdr.key().c_str(),
+                               (int)hdr.value_size(),
+                               (char const*)hdr.value());
+                    else
+                        printf(" Header:  %s = NULL\n", hdr.key().c_str());
+                }
+            }
+            printf("%.*s\n", static_cast<int>(message->len()), static_cast<char const*>(message->payload()));
+            break;
+
+        case RdKafka::ERR__PARTITION_EOF:
+            /* Last message */
+            if (exit_eof) {
+                run = 0;
+            }
+            break;
+
+        case RdKafka::ERR__UNKNOWN_TOPIC:
+        case RdKafka::ERR__UNKNOWN_PARTITION:
+            std::cerr << "Consume failed: " << message->errstr() << std::endl;
+            run = 0;
+            break;
+
+        default:
+            /* Errors */
+            std::cerr << "Consume failed: " << message->errstr() << std::endl;
+            run = 0;
+    }
+
+    std::cout << "<Ctrl+C> suivi de <Entrée> pour arrêter le consumer" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    std::string brokers = "localhost";
+    std::string errstr;
+    if (argc < 2) {
+        std::cerr << "ERROR : missing topic_str" << std::endl;
+        exit(1);
+    }
+    std::string topic_str = argv[1];
+    // int32_t partition = RdKafka::Topic::PARTITION_UA;
+    int32_t partition = 0;  // NDM : ça n'a pas l'air de marcher avec PARTITION_UA...
+    int64_t start_offset = RdKafka::Topic::OFFSET_BEGINNING;
+
+    /*
+     * Create configuration objects
+     */
+    RdKafka::Conf* conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+    RdKafka::Conf* tconf = RdKafka::Conf::create(RdKafka::Conf::CONF_TOPIC);
+
+    /*
+     * Set configuration properties
+     */
+    conf->set("metadata.broker.list", brokers, errstr);
+
+    signal(SIGINT, sigterm);
+    signal(SIGTERM, sigterm);
+
+    conf->set("enable.partition.eof", "true", errstr);
+
+    /*
+     * Create consumer using accumulated global configuration.
+     */
+    RdKafka::Consumer* consumer = RdKafka::Consumer::create(conf, errstr);
+    if (!consumer) {
+        std::cerr << "Failed to create consumer: " << errstr << std::endl;
+        exit(1);
+    }
+
+    std::cout << "% Created consumer " << consumer->name() << std::endl;
+
+    /*
+     * Create topic handle.
+     */
+    RdKafka::Topic* topic = RdKafka::Topic::create(consumer, topic_str, tconf, errstr);
+    if (!topic) {
+        std::cerr << "Failed to create topic: " << errstr << std::endl;
+        exit(1);
+    }
+
+    /*
+     * Start consumer for topic+partition at start offset
+     */
+    RdKafka::ErrorCode resp = consumer->start(topic, partition, start_offset);
+    if (resp != RdKafka::ERR_NO_ERROR) {
+        std::cerr << "Failed to start consumer: " << RdKafka::err2str(resp) << std::endl;
+        exit(1);
+    }
+
+    /*
+     * Consume messages
+     */
+    while (run) {
+        int timeout_ms = 1000;
+        RdKafka::Message* msg = consumer->consume(topic, partition, timeout_ms);
+        msg_consume(msg);
+        delete msg;
+        int no_timeout = 0;
+        consumer->poll(no_timeout);
+    }
+
+    /*
+     * Stop consumer
+     */
+    consumer->stop(topic, partition);
+
+    int poll_timeout_ms = 1000;
+    consumer->poll(poll_timeout_ms);
+
+    delete topic;
+    delete consumer;
+
+    delete conf;
+    delete tconf;
+
+    /*
+     * Wait for RdKafka to decommission.
+     * This is not strictly needed (when check outq_len() above), but
+     * allows RdKafka to clean up all its resources before the application
+     * exits so that memory profilers such as valgrind wont complain about
+     * memory leaks.
+     */
+    int wait_destroyed_timeout_ms = 5000;
+    RdKafka::wait_destroyed(wait_destroyed_timeout_ms);
+
+    return 0;
+}
